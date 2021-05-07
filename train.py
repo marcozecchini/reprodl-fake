6fd4c3ba0ce556e49949ca5d01e749670dca1ca3
Marco Zecchini <zecchini@diag.uniroma1.it>
2021-05-07 12:08:27 +0200

    added hydra

--- train.py
+++ train.py
@@ -11,8 +11,12 @@ import pytorch_lightning as pl
 from pathlib import Path
 import matplotlib.pyplot as plt
 import pandas as pd
+import hydra
+from hydra.utils import get_original_cwd
+from omegaconf import DictConfig, OmegaConf
 
-
+import logging
+logger = logging.getLogger(__name__)
 
 # everytime you want to work with data in Pytorch we need to use Dataset object
 class ESC50Dataset(torch.utils.data.Dataset):
@@ -26,11 +30,11 @@ class ESC50Dataset(torch.utils.data.Dataset):
         # Resample --> Melspectrogram --> AmplitudeToDB
 
         self.path = path
-        self.csv = pd.read_csv(datapath /  Path('meta/esc50.csv'))
+        self.csv = pd.read_csv(self.path /  Path('meta/esc50.csv'))
         self.csv = self.csv[self.csv['fold'].isin(folds)]
         
         self.resample = torchaudio.transforms.Resample(orig_freq=44100, new_freq=sample_rate)
-        self.melspec = torchaudio.transforms.MelSpectrogram(sample_rate=sr)
+        self.melspec = torchaudio.transforms.MelSpectrogram(sample_rate=sample_rate)
         self.db = torchaudio.transforms.AmplitudeToDB()
         
     #function to index with [] square brackets
@@ -52,19 +56,20 @@ class ESC50Dataset(torch.utils.data.Dataset):
 
 class AudioNet(pl.LightningModule):
  
-    def __init__(self, n_classes = 50, base_filters = 32): # or even 16 as base filters without GPU
+    def __init__(self, hparams): # or even 16 as base filters without GPU
         super().__init__()
-        self.conv1 = nn.Conv2d(1, base_filters, 11, padding=5)
-        self.bn1 = nn.BatchNorm2d(base_filters)
-        self.conv2 = nn.Conv2d(base_filters, base_filters, 3, padding=1)
-        self.bn2 = nn.BatchNorm2d(base_filters)
+        self.hparams = hparams
+        self.conv1 = nn.Conv2d(1, hparams.base_filters, 11, padding=5)
+        self.bn1 = nn.BatchNorm2d(hparams.base_filters)
+        self.conv2 = nn.Conv2d(hparams.base_filters, hparams.base_filters, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(hparams.base_filters)
         self.pool1 = nn.MaxPool2d(2)
-        self.conv3 = nn.Conv2d(base_filters, base_filters * 2, 3, padding=1)
-        self.bn3 = nn.BatchNorm2d(base_filters * 2)
-        self.conv4 = nn.Conv2d(base_filters * 2, base_filters * 4, 3, padding=1)
-        self.bn4 = nn.BatchNorm2d(base_filters * 4)
+        self.conv3 = nn.Conv2d(hparams.base_filters, hparams.base_filters * 2, 3, padding=1)
+        self.bn3 = nn.BatchNorm2d(hparams.base_filters * 2)
+        self.conv4 = nn.Conv2d(hparams.base_filters * 2, hparams.base_filters * 4, 3, padding=1)
+        self.bn4 = nn.BatchNorm2d(hparams.base_filters * 4)
         self.pool2 = nn.MaxPool2d(2)
-        self.fc1 = nn.Linear(base_filters * 4, n_classes)
+        self.fc1 = nn.Linear(hparams.base_filters * 4, hparams.n_classes)
  
     def forward(self, x):
         x = self.conv1(x)
@@ -93,30 +98,34 @@ class AudioNet(pl.LightningModule):
         return loss
     
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams.optim.lr)
         return optimizer
 
+@hydra.main(config_path="configs", config_name='default')
+def train(cfg: DictConfig):
 
-def train():
-    train_data = ESC50Dataset(folds=[1,2,3])
-    val_data = ESC50Dataset(folds=[4])
-    test_data = ESC50Dataset(folds=[5])
+    logger.info(OmegaConf.to_yaml(cfg))
 
-    # batch size has influence on convergence but also on memory
-    train_loader = torch.utils.data.DataLoader(train_data, batch_size=8, shuffle=True)
-
-    # batch size has influence on convergence but also on memory
-    val_loader = torch.utils.data.DataLoader(val_data, batch_size=8, shuffle=True)
+    path = Path(get_original_cwd()) / Path(cfg.data.path)
+    train_data = ESC50Dataset(path=path, sample_rate=cfg.data.sample_rate, folds=cfg.data.train_folds)
+    val_data = ESC50Dataset(path=path, sample_rate=cfg.data.sample_rate, folds=cfg.data.val_folds)
+    test_data = ESC50Dataset(path=path, sample_rate=cfg.data.sample_rate, folds=cfg.data.test_folds)
 
     # batch size has influence on convergence but also on memory
-    test_loader =     torch.utils.data.DataLoader(test_data, batch_size=8, shuffle=True)
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=cfg.data.batch_size, shuffle=True)
+
+    # batch size has influence on convergence but also on memory
+    val_loader = torch.utils.data.DataLoader(val_data, batch_size=cfg.data.batch_size, shuffle=True)
+
+    # batch size has influence on convergence but also on memory
+    test_loader =     torch.utils.data.DataLoader(test_data, batch_size=cfg.data.batch_size, shuffle=True)
 
     pl.seed_everything(0) # always do it
-    audionet = AudioNet()
-    trainer = pl.Trainer(max_epochs=1)
+    audionet = AudioNet(cfg.model)
+    trainer = pl.Trainer(**cfg.trainer) # Pass each key of the dictionary as a value
     trainer.fit(audionet, train_loader, val_loader)
     trainer.test(audionet, test_loader)
 
 
-if "__main__":
+if __name__ == "__main__":
     train()
